e954a4d3921c5b2fb193878b73b266faf0e8fb1f
Sally Mayweather <sal1501w@gmail.com>
2025-10-25 11:09:54 -0300

    meow

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.identity = "data/saves"
+    t.version = "11.5"
+    t.console = false
+    t.gammacorrect = false
+    t.window.title = "deadweight"
+    t.window.icon = "/assets/images/dw_logo_sun.png"
+end
--- main.lua
+++ main.lua
@@ -1,16 +1,19 @@
 function love.load()
-     love.graphics.setDefaultFilter("nearest", "nearest")
+    love.graphics.setDefaultFilter("nearest", "nearest")
 
-    love.window.setTitle("deadweight")
-    love.window.setIcon(love.image.newImageData("/assets/images/dw_logo_sun.png"))
-    local font = love.graphics.newFont("/assets/fonts/dotumche-pixel.ttf", 14)
+    font = love.graphics.newFont("/assets/fonts/dotumche-pixel.ttf", 26)
     love.graphics.setFont(font)
 end
 
 function love.update(dt)
-
+    fps = love.timer.getFPS()
 end
 
 function love.draw()
-    love.graphics.print("mrrp meow", 400, 300)
+    local text = fps .. " FPS"
+
+    love.graphics.setColor(0.6, 0, 0)
+    love.graphics.print(text, love.graphics:getWidth() - 10, 10, 0, 1, 1, font:getWidth(text) - 2, -2)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.print(text, love.graphics:getWidth() - 10, 10, 0, 1, 1, font:getWidth(text))
 end
